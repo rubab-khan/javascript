c7a3168208014156ad08df0d46de74ea1c17824a
rubab-khan <rubab9146@gmail.com>
2024-12-29 09:36:44 -0800

    firstcommit

---
+++ main.js
@@ -0,0 +1,42 @@
+alert("welcome to javascript");
+var Name=prompt ("whatisyourname?");
+alert("hello"+""+Name);
+var firstName = "Rubab";
+var lastName ="Khan";
+var fullName = firstName+""+lastName;
+alert(fullName);
+var a = prompt("enter first no");
+var b = prompt("enter second no");
+var c = a + b;
+alert(c);
+var age ="28";
+var BirthYear ="1996";
+var CurrentYear ="2024";
+var calculatedAge=CurrentYear-BirthYear;
+alert("your age is"+calculatedAge);
+var hobby = prompt("your favourite hobbies");
+alert("your fav hobby is" + hobby + "" )
+var lenght = prompt ("enter the lenght of rectangle");
+var width = prompt ("enter the width of rectangle");
+var area = lenght * width;
+alert("the area of the rectangle is" + area);
+var myAge = prompt("enter your age");
+var friendsAge = prompt("enter your friends age")
+alert("my age is" + myAge + "and my friends age is" +friendsAge);
+currentyear = prompt("please enter current year");
+birthyear = prompt("please enter your birth year");
+age= currentyear - birthyear;
+alert("you are" + age + "years old");
+var number1 = prompt("enter your first number");
+var number2 = prompt("enter your second number");
+sum_total = number1 + number2
+difference_total = number1 - number2;
+product_total = number1 / number2;
+quotient_total = number1 % number2;
+ramainder_total = number1 % number2;
+alert("sum" + sum_total);
+alert("difference" + difference_total);
+alert("product" + product_total);
+alert("quotient" + quotient_total);
+alert("remainder" + remainder_total);
+
